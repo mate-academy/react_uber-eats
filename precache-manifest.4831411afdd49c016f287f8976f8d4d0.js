d13bc13db16fe157086869e2e76c6dfd81163cf1
mgrinko <glukym@gmail.com>
2019-08-19 08:10:13 +0300

    Updates

---
+++ precache-manifest.4831411afdd49c016f287f8976f8d4d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85071e27399260ecefa923f7ea177676",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "0b742b5127f0b3a2c6a3",
+    "url": "/react_uber-eats/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/react_uber-eats/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "0b742b5127f0b3a2c6a3",
+    "url": "/react_uber-eats/static/js/main.c52d50dd.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
---
+++ static/js/main.c52d50dd.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var a=t(0),c=t.n(a),r=t(2),o=t.n(r),u=(t(8),function(){return c.a.createElement("div",{className:"App"},c.a.createElement("h1",null,"React Uber eats"))});o.a.render(c.a.createElement(u,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.c52d50dd.chunk.js.map
