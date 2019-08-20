999b90357462bc1abf527a3ed6ac12f78758ea75
Nastia Krat <nastyakpi1995@gmail.com>
2019-08-20 08:35:48 +0300

    css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,38 @@ import './App.css';
 
 const App = () => (
   <div className="App">
-    <h1>React Uber eats</h1>
+    <header className="header">
+      <div className="header__wrapp">
+        <div className="header__logo">
+          <div className="header__logo__uber" />
+          <div className="header__logo__eats" />
+        </div>
+        <form className="header__wrapp__form">
+          <div className="header__wrapper__location">
+            <div className="header__location" />
+            <input
+              type="text"
+              placeholder="Kyiv"
+            />
+          </div>
+          <div className="header__delivery">
+            <div className="header__delivery-img" />
+            <select>
+              <option>Deliver now</option>
+              <option>Schedule for later</option>
+            </select>
+          </div>
+          <div>
+            <div className="header__search" />
+            <input
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+          <button type="submit">Sign in</button>
+        </form>
+      </div>
+    </header>
   </div>
 );
 
