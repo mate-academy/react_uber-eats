5f2cbe3c49c0c239e04936ce13f1663044e2141a
unknown <mariia>
2019-08-27 20:10:38 +0300

    solution

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,76 +3,68 @@ import React from 'react';
 const Footer = () => (
   <footer class="footer">
     <section class="footer__section">
-    <div class="footer__logo">
-      <a href="#">
-        <img src="images/icons/white-logo.svg" alt="logo" />
-      </a>
-    </div>
-    <hr class="hr" />
 
-    <div class="footer__nav">
-      <div class="footer__column">
-        <form class="footer__nav-lang">
-          <select class="footer__nav-lang-input">
-            <option class="footer__nav-lang-input" value="РУССКИЙ">РУССКИЙ</option>
-            <option class="footer__nav-lang-input" selected value="ENGLISH">ENGLISH</option>
-            <option class="footer__nav-lang-input" value="УКРАЇНСЬКА">УКРАЇНСЬКА</option>
-          </select>
-        </form>
-
-        <p class="footer__nav-name">
-          <span class="footer__nav-name-hash">#</span>
-          <span class="footer__nav-name-name">UberEats</span>
-        </p>
-
-        <div class="footer__social">
+      <div className="footer__logo-section">
+        <div class="footer__logo">
           <a href="#">
-            <img src="images/icons/facebook.svg" alt="facebook-icon" />
+            <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/12c47a69e1022b581a7e823e9bd45466.svg" alt="logo" />
           </a>
-          <a class="footer__social--twitter" href="#">
-            <img src="images/icons/twitter.svg" alt="twitter-icon" />
+        </div>
+
+        <div class="footer__app">
+          <a class="appstore" href="#/" target="_blank">
+            <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/d0558d91063038236b60e3ef71fdc1fd.svg" alt="appstore-icon" />
           </a>
-          <a class="footer__social--instagram" href="#">
-            <img src="images/icons/instagram.svg" alt="instagram-icon" />
+
+          <a class="gplay" href="#/" target="_blank">
+            <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/cf6dad406fdfdcd290fd40de9008ae50.png" alt="googleplay-icon" />
           </a>
         </div>
       </div>
 
       <div class="footer__column">
-        <a href="#/">About UberEats</a>
-        <a href="#/">Become a Delivery Partner</a>
-        <a href="#/">Become a Restaurant Partner</a>
+        <a href="#/">About Uber Eats</a>
+        <a href="#/">Read our blog</a>
+        <a href="#/">Buy gift cards</a>
+        <a href="#/">Sign up to deliver</a>
+        <a href="#/">Add your restaurant</a>
       </div>
 
       <div class="footer__column">
-        <a href="#/">See all cities</a>
-        <a href="#/">Pricing</a>
-        <a href="#/">Help</a>
-        <a href="#/">FAQs</a>
+        <a href="#/">Get Help</a>
+        <a href="#/">Read FAQs</a>
+        <a href="#/">View all cities</a>
+        <a href="#/">
+          <img src='https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/2601cb43facfcd27a8d88d727dce9bb5.svg' />
+          English
+        </a>
       </div>
-    </div>
+    </section>
     <hr class="hr" />
 
-    <div class="footer__app">
-      <a class="appstore" href="#/" target="_blank">
-        <img src="images/download_apple.png" alt="appstore-icon" />
-      </a>
+    <div className="footer__logo-section-mobile">
+          <a class="appstore" href="#/" target="_blank">
+            <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/d0558d91063038236b60e3ef71fdc1fd.svg" alt="appstore-icon" />
+          </a>
 
-      <a class="gplay" href="#/" target="_blank">
-        <img src="images/google_play.png" alt="googleplay-icon" />
-      </a>
-    </div>
-    <hr class="hr"/>
+          <a class="gplay" href="#/" target="_blank">
+            <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/cf6dad406fdfdcd290fd40de9008ae50.png" alt="googleplay-icon" />
+          </a>
+      </div>
+      
+      <hr class="hr-mobile" />
 
     <div class="footer__rights">
       <p>© 2019 Uber Technologies Inc.</p>
-      <a class="link" href="#/">Privacy</a>
-      <a class="link" href="#/">Terms</a>
+      <section className="footer__rights-info">
+        <a class="link" href="#/">Privacy Policy</a>
+        <a class="link" href="#/">Terms of Use</a>
+        <a class="link" href="#/">Pricing</a>
+      </section>
     </div>
 
     <a href="#" class="arrow-to-top">
     </a>
-    </section>
   </footer>
 )
 
--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ ReactDOM.render(<App />, document.getElementById('root'));
 function consoleBG() {
   if ($(window).scrollTop() == 0) {
     $('.header__top').removeClass('header__fixed');
+    $('.arrow-to-top').removeClass('arrow-displayed');
   } else {
     $('.header__top').addClass('header__fixed');
+    $('.arrow-to-top').addClass('arrow-displayed');
   }
 }
 consoleBG();
