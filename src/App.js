6d5149c216a74a34e4ff53476f86e3bfe1b9b02c
Nastia Krat <nastyakpi1995@gmail.com>
2019-08-31 13:45:35 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+// import interactionPlugin from '@fullcalendar/interaction';
+
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <h1>React Uber eats</h1>
-  </div>
-);
+export default class DemoApp extends React.Component {
+  state = {
+    calendar: '',
+  }
 
-export default App;
+  render() {
+    return (
+      <div>
+        <h1 className="calendar">
+          Calendar View
+          {this.state.calendar}
+        </h1>
+        <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
+
+      </div>
+    );
+  }
+}
