ce66c21c4c8d6d5e2191de403af9df1a7b4be970
VladyslavSak <iblackvour@gmail.com>
2019-10-09 20:47:55 +0300

    Implement layout for RestaurantCard and make grid for restaurants-list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import './styles/index.scss';
 
 import { RestaurantListPage } from './components/RestaurantListPage';
 
--- src/components/RestaurantListPage/RestaurantListPage.js
+++ src/components/RestaurantListPage/RestaurantListPage.js
@@ -1,5 +1,9 @@
-/* eslint-disable import/prefer-default-export */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './RestaurantListPage.scss';
+import { RestaurantCard } from '../RestaurantCard/RestaurantCard';
+
+const DEFAULT_ETA_RANGE = '20 - 30 min';
 
 export class RestaurantListPage extends Component {
   componentDidMount() {
@@ -12,11 +16,37 @@ export class RestaurantListPage extends Component {
     const { restaurantsData } = this.props;
 
     return (
-      <div>
-        <pre>
-          {JSON.stringify(restaurantsData, null, 4)}
-        </pre>
+      <div className='restaurants-list'>
+        {restaurantsData.map(restaurant => {
+          const {
+            uuid,
+            title,
+            heroImageUrl,
+            categories,
+            etaRange,
+          } = restaurant;
+
+          return (
+            <RestaurantCard
+              key={uuid}
+              uuid={uuid}
+              title={title}
+              imageUrl={heroImageUrl}
+              categories={categories}
+              etaRange={etaRange ? etaRange.text : DEFAULT_ETA_RANGE}
+            />
+          );
+        })}
       </div>
     );
   }
 }
+
+RestaurantListPage.propTypes = {
+  restaurantsData: PropTypes.arrayOf(PropTypes.shape({})),
+  loadRestaurants: PropTypes.func.isRequired,
+};
+
+RestaurantListPage.defaultProps = {
+  restaurantsData: [],
+};
