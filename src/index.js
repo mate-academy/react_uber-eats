8be6962194cb8dbe0833f0bb7be23e50c88fd073
igaryok <igor.sobko@gmail.com>
2019-10-30 14:35:40 +0200

    change to HashRoute

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { App } from './components/App';
@@ -10,9 +10,9 @@ import './styles/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 
   document.getElementById('root')
