94b2931c0f00741b923c2684bd1ab9965248d479
youvovk <wer3wollf@gmail.com>
2019-12-01 21:39:24 +0200

    add modal window content

--- src/components/ModalWindow/ModalWindow.js
+++ src/components/ModalWindow/ModalWindow.js
@@ -7,31 +7,36 @@ export const ModalWindow = () => {
   return (
     <div className="modal-window__container modal-window">
       <div className="modal-window__content menu">
-        <img src="" alt="" />
-        <h2 className="menu__title">STAR CHEESE Burger</h2>
-        <p className="menu__recipe">Cheddar cheese, Boston lettuce, tomatoes, onion, pickles and firm sauce 275 g.</p>
-        <div className="menu__item">
-          <h3>Double Meat</h3>
-          <p>Choose up to 1</p>
+        <img src="./images/modal-img.svg" alt="" className="modal-window__img" />
+        <div className="menu__title-wrapper">
+          <h2 className="menu__title">STAR CHEESE Burger</h2>
+          <p className="menu__text-grey">Cheddar cheese, Boston lettuce, tomatoes, onion, pickles and firm sauce 275 g.</p>
         </div>
-        <div className="menu__points points">
-          <div className="points__name">
-            <input type="checkbox" />
+
+        <div className="menu__background">
+          <h3 className="menu__text-black">Double Meat</h3>
+          <p className="menu__text-grey">Choose up to 1</p>
+        </div>
+        <div className="menu__points point">
+          <div className="point__name">
+            <input type="checkbox" className="point__checkbox" />
             <p>Cheddar</p>
           </div>
-          <p>+£0.68</p>
+          <p className="point__price">+£0.68</p>
         </div>
 
-        <p>Special instructions</p>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <p className="menu__text-black menu__background">Special instructions</p>
+        <textarea className="menu__textarea" placeholder="Leave a note to kitchen" name="modal" id="" cols="30" rows="10" />
 
         <div className="menu__order-container order">
-          <div className="order__minus"></div>
-          <p>1</p>
-          <div className="order__plus"></div>
-          <div className="order__add">
-            <p>Add 1 to order</p>
-            <p>139,00 UAH</p>
+          <div className="order__toggle toggle">
+            <div className="toggle__minus" />
+            <p>1</p>
+            <div className="toggle__plus" />
+          </div>
+          <div className="order__add add">
+            <p className="add-center">Add 1 to order</p>
+            <p className="add-right">139,00 UAH</p>
           </div>
         </div>
       </div>
