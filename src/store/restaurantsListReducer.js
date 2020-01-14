47dc4b56b5b04aa7448c957b9729a760ac7db8e3
Ihor Stavratii <igor.stavratiy@gmail.com>
2020-01-14 09:55:57 +0200

    minor fix

--- src/store/restaurantsListReducer.js
+++ src/store/restaurantsListReducer.js
@@ -1,17 +1,9 @@
-import { getRestaurantsData } from '../Api';
-
 const SET_RESTAURANTS_LIST = 'SET_RESTAURANTS_LIST';
 
 export const setRestaurantsList = restaurantsList => ({
   type: SET_RESTAURANTS_LIST,
   restaurantsList,
 });
-
-export const loadRestaurantsList = () => async(dispatch) => {
-  const restaurants = await getRestaurantsData();
-
-  dispatch(setRestaurantsList(restaurants.data));
-};
 
 const restaurantsListReducer = (state = null, action) => {
   switch (action.type) {
