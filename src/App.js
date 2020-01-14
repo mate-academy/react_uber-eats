57349fdca2582f4c091dbafe92827d0d3e755f87
Liliya Taran <liliyataran2017@gmail.com>
2020-01-15 01:48:09 +0200

    cards layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import './App.css';
 import { store } from './store';
+import './styles/index.scss';
 import { RestaurantsListPage } from './components/RestaurantsListPage';
 
 const App = () => (
   <Provider store={store}>
     <div className="page">
-      <RestaurantsListPage />
+      <main className="main-content">
+        <RestaurantsListPage />
+      </main>
     </div>
   </Provider>
 );
--- src/components/RestaurantsListPage/RestaurantsListPage.js
+++ src/components/RestaurantsListPage/RestaurantsListPage.js
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
 import Proptypes from 'prop-types';
+import { RestaurantCard } from '../RestaurantCard';
+import './RestaurantsListPage.scss';
+
+const DEFAULT_ETA_RANGE = '20 - 30 min';
 
 export const RestaurantsListPage = ({ restaurantsData, loadRestaurants }) => {
   useEffect(() => {
@@ -7,10 +11,27 @@ export const RestaurantsListPage = ({ restaurantsData, loadRestaurants }) => {
   }, []);
 
   return (
-    <div>
-      <pre>
-        {JSON.stringify(restaurantsData, null, 4)}
-      </pre>
+    <div className="restaurant-list">
+      {restaurantsData.map((restaurant) => {
+        const {
+          uuid,
+          title,
+          heroImageUrl,
+          categories,
+          etaRange,
+        } = restaurant;
+
+        return (
+          <RestaurantCard
+            key={uuid}
+            uuid={uuid}
+            title={title}
+            imageUrl={heroImageUrl}
+            categories={categories}
+            etaRange={etaRange ? etaRange.text : DEFAULT_ETA_RANGE}
+          />
+        );
+      })}
     </div>
   );
 };
