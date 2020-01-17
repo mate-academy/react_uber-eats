79e44906cf86ec14bd0e6b834b6256cdd4fbf39f
vvxforce <vvxforce@gmail.com>
2020-01-17 06:17:25 +0200

    List without styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import store from './store/index';
 import { Enchanced as RestaurantsListPage }
   from './store/RestaurantsListPage/index';
+import './App.css';
 
 const App = () => (
   <Provider store={store}>
